a5712c7a25ebf965ad74387812c1c67bebe852fb
xuankha1011 <nguyenxuankha5371@gmail.com>
2024-10-19 13:56:26 +0700

    fix chart

--- src/components/ui/admin-component-layout/render-chart.tsx
+++ src/components/ui/admin-component-layout/render-chart.tsx
@@ -2,13 +2,13 @@
 import { Statistic } from "@/types"
 import { CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 
-// Function to format numbers as VND currency with abbreviation
 const formatVND = (value: number) => {
     if (value >= 1_000_000) {
         return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND', notation: 'compact' }).format(value)
     }
     return new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(value)
 }
+const defaultFormatter = (value: number) => value.toString()
 
 export const renderChart = (statistic: Statistic | null, isMonthlyView: boolean) => {
     const chartData = isMonthlyView ? statistic?.viewLineChartByMonth ?? [] : statistic?.viewLineChart ?? []
@@ -18,7 +18,7 @@ export const renderChart = (statistic: Statistic | null, isMonthlyView: boolean)
             <LineChart data={chartData}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey={isMonthlyView ? "day" : "name"} tick={{ fontSize: 12 }} />
-                <YAxis width={100} tickFormatter={formatVND} />
+                <YAxis width={100} tickFormatter={isMonthlyView ? formatVND : defaultFormatter} />
                 <Tooltip formatter={(value: number) => formatVND(value)} />
                 <Legend />
                 <Line type="monotone" dataKey="users" stroke="#8884d8" activeDot={{ r: 8 }} />
